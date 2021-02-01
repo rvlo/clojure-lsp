ab17d495f91813e1e41c67f392f75d46f9fdda2a
Eric Dallo <ericdallo06@hotmail.com>
2021-02-01 00:55:54 -0300

    WIP migrate to deps.edn

--- project.clj
+++ project.clj
@@ -1,22 +1,6 @@
-(require '[clojure.edn :as edn])
-
-(def +deps+ (-> "deps.edn" slurp edn/read-string))
-
-(defn deps->vec [deps]
-  (keep (fn [[dep {:keys [:mvn/version exclusions]}]]
-          (when version
-            (cond-> [dep version]
-              exclusions (conj :exclusions exclusions))))
-        deps))
-
-(def dependencies
-  (deps->vec (:deps +deps+)))
-
 (defproject clojure-lsp :project/git-tag
-  :dependencies ~dependencies
   :jvm-opts ^:replace ["-Xmx2g" "-server"]
   :main clojure-lsp.main
-  :java-source-paths ["src-java"]
   :resource-paths ["resources"]
   :javac-options ["-implicit:none"]
   :plugins [[me.arrdem/lein-git-version "2.0.8"]]
@@ -26,8 +10,4 @@
   :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]
                              [lein-binplus "0.6.6"]]
                    :bin {:name "clojure-lsp"}}
-             :test {:test-selectors {:focused :focused}
-                    :dependencies [[pjstadig/humane-test-output "0.9.0"]]
-                    :injections [(require 'pjstadig.humane-test-output)
-                                 (pjstadig.humane-test-output/activate!)]}
              :uberjar {:aot :all}})
